5b896cc21c1457f7a271d692a1eb5557f28763b8
Sohel Ansari <90556891+sohel22z@users.noreply.github.com>
2022-11-26 22:52:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import './App.css';
+import "./App.css";
 import React, { useState } from "react";
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import Alert from './components/Alert';
-import Footer from './components/Footer';
-
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+import Footer from "./components/Footer";
 
 function App() {
   const [alert, setAlert] = useState(null);
-
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
       setAlert(null);
     }, 1500);
-  }
-
+  };
   return (
     <>
       <Navbar title="TextEditor" />
       <Alert alert={alert} />
       <TextForm showAlert={showAlert} />
-      <Footer/>
+      <Footer />
     </>
   );
 }

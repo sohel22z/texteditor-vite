a54fab56ab51a1d5fe87f712b250e94c11ca693c
Sohel Ansari <90556891+sohel22z@users.noreply.github.com>
2022-11-26 23:54:48 +0530

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import iconOne from "../assets/img/instagram.png";
 import iconTwo from "../assets/img/linkedin.png";
 
